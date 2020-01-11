392fd128826b1c9a9de3cb6e998f80e5379df348
amarantini <38065325+amarantini@users.noreply.github.com>
2020-01-11 10:03:35 -0800

    Update chatTerminator.py

--- chatTerminator.py
+++ chatTerminator.py
@@ -3,6 +3,24 @@ import json
 
 data = json.load(open("data.json", encoding="utf-8"))
 
+quote = data["famous"] # a 代表前面垫话，b代表后面垫话
+before = data["before"] # 在名人名言前面弄点废话
+after = data['after']  # 在名人名言后面弄点废话
+between = data['between'] # 代表文章主要废话来源
+
+repeat = 2
+
+def shuffleThrough(lst):
+    global repeat
+    pond = list(lst) * repeat
+    while True:
+        random.shuffle(pond)
+        for element in pond:
+            yield element
+
+nextBetween = shuffleThrough(between)
+nextQuote = shuffleThrough(quote)
+
 def generator(title, length=640):
     """
     :param title: //
@@ -13,13 +31,29 @@ def generator(title, length=640):
     while len(body) < length:
         num = random.randint(0, 100)
         if num < 10:
-            body += "\r\n"
-        elif num < 20:
-            body += random.choice(data["famous"]) \
+            body += nextQuote \
                 .replace('a', random.choice(data["before"])) \
                 .replace('b', random.choice(data['after']))
         else:
-            body += random.choice(data["bosh"])
+            body += nextBetween
         body = body.replace("x", title)
 
     return body
+
+
+
+if __name__ == "__main__":
+    xx = input("Please input the the chat:")
+    for x in xx:
+        tmp = str()
+        while ( len(tmp) < 6000 ) :
+            分支 = random.randint(0,100)
+            if 分支 < 5:
+                tmp += 另起一段()
+            elif 分支 < 20 :
+                tmp += 来点名人名言()
+            else:
+                tmp += next(下一句废话)
+        tmp = tmp.replace("x",xx)
+        print(tmp)
+
